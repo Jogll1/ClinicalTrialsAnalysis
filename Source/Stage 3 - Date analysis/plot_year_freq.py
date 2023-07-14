4e36ca5808556a1325a8550fcf1a7f76a75f04e9
JoelLuckett <joelluckett@icloud.com>
2023-07-14 17:14:36 +0100

    working on chronographic data view

--- Source/Stage 3 - Date analysis/plot_year_freq.py
+++ Source/Stage 3 - Date analysis/plot_year_freq.py
@@ -21,6 +21,7 @@ start_years_list = df["Start Date"].astype(str).to_list()
 # get the conditions in mapped_conditions
 map_cond_list = [] # list of all conditions in mapped_condition
 map_spec_list = [] # list of all specialities in mapped_condition
+print("Get unqiue conditions and specialisations")
 with open(map_cond_path, 'r', encoding='utf-8') as f:
     for line in f:
         strip_line_cond = line.split(':')[0]
@@ -30,6 +31,7 @@ with open(map_cond_path, 'r', encoding='utf-8') as f:
             map_spec_list.append(strip_line_spec)
 
 # write conditions and start year to a text file
+print("Write to condition_with_year.txt")
 with open(output_txt_path1, 'w', encoding='utf-8') as f_out:
     for i in range(0, len(conditions_list)):
         #split conditions seperated by '|' and add to list 
@@ -40,6 +42,7 @@ with open(output_txt_path1, 'w', encoding='utf-8') as f_out:
 
 # check each condition and remove ones which aren't in mapped_conditions
 # then create new file and add the specialty paired with the year
+print("Write to spec_with_year.txt")
 with open(output_txt_path1, "r", encoding='utf-8') as f_in, open(output_txt_path2, "w", encoding='utf-8') as f_out:
     for line in f_in:
         values = line.split(':')
@@ -50,3 +53,59 @@ with open(output_txt_path1, "r", encoding='utf-8') as f_in, open(output_txt_path
             f_out.write(spec + ":" + year)
 
 # now collect all repeated instances of speciality:year with freqeuncies
+spec_and_year_tokens = []
+spec_and_year_frequency = []
+print("Write lists for frequencies of spec and year")
+with open(output_txt_path2, "r", encoding='utf-8') as f_in:
+    for line in f_in:
+        if line not in spec_and_year_tokens:
+            spec_and_year_tokens.append(line)
+            spec_and_year_frequency.append(1)
+        else:
+            # increment the frequencies
+            spec_and_year_frequency[spec_and_year_tokens.index(line)] += 1
+
+# clean spec_and_year_tokens
+print("Clean spec_and_year_tokens")
+for i in range(0, len(spec_and_year_tokens)):
+    spec_and_year_tokens[i] = spec_and_year_tokens[i].strip('"').strip('\n')
+
+# add these values to a csv
+print("Write to spec_year_frequencies.csv")
+df = pd.DataFrame({'Specialisation-Year': spec_and_year_tokens, 'Frequency': spec_and_year_frequency})
+sorted_df = df.sort_values('Frequency', ascending=False)
+sorted_df.to_csv(output_txt_path3, index=False)
+
+# now generate a 2d array csv
+print("Write to nested dictionary object")
+# the nested dictionary should be like
+# data = {
+#   'oncology': {
+#       2019: 405,
+#       2020: 236,
+#       2021: 357
+#   },
+#   'cardiology': {
+#       2019: 564,
+#       2020: 345,
+#       2021: 235
+#   }
+# }
+data_for_plotting = {}
+
+# read the CSV file using pandas
+# df = pd.read_csv(output_txt_path3)
+
+# # iterate over each row in the dataframe
+# for index, row in df.iterrows():
+#     values = row['Specialisation-Year'].split(':')
+#     spec = values[0]
+#     year = values[1]
+#     frequency = int(row['Frequency'])
+
+#     if spec not in data_for_plotting:
+#         data_for_plotting[spec] = {}
+
+#     data_for_plotting[spec][year] = frequency
+
+# print(data_for_plotting)
--- Source/Stage 3 - Date analysis/plotter.py
+++ Source/Stage 3 - Date analysis/plotter.py
@@ -1,16 +1,65 @@
-# test plotter
+# # test plotter
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # xpoints = np.array([0, 6])
+# # ypoints = np.array([0, 250])
+
+# xpoints1 = np.array([2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015])
+
+# ypoints1 = np.array([5, 6, 3, 6, 24, 12, 14, 19, 22, 34, 20, 15, 5, 67, 45, 32])
+# ypoints2 = np.array([2, 34, 24, 17, 4, 6, 7, 13, 17, 26, 36, 31, 25, 7, 2, 3])
+
+# # marker|line|color
+# plt.plot(xpoints1, ypoints1, marker='o', c='#4caf50')
+# plt.plot(xpoints1, ypoints2, marker='o', c='#f4f5ad')
+# plt.show()
+
 import matplotlib.pyplot as plt
-import numpy as np
 
-# xpoints = np.array([0, 6])
-# ypoints = np.array([0, 250])
+data = {
+    'Oncology': {
+        2021: 5025,
+        2022: 4580,
+        2020: 4484,
+        2019: 4258,
+        2018: 3692,
+        2017: 3489,
+        2016: 3092,
+        2023: 3033,
+        2015: 2768
+    },
+    'Neurology': {
+        2021: 3511,
+        2022: 3186,
+        2019: 2885,
+        2018: 2792
+    },
+    'Psychiatry': {
+        2022: 3501,
+        2021: 3423,
+        2020: 2924,
+        2019: 2844
+    },
+    'Infectious diseases': {
+        2020: 3299
+    },
+    'Cardiology': {
+        2021: 2778
+    }
+}
 
-xpoints1 = np.array([2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015])
+colors = ['blue', 'green', 'red', 'orange', 'purple']  # Assign colors to specialties
 
-ypoints1 = np.array([5, 6, 3, 6, 24, 12, 14, 19, 22, 34, 20, 15, 5, 67, 45, 32])
-ypoints2 = np.array([2, 34, 24, 17, 4, 6, 7, 13, 17, 26, 36, 31, 25, 7, 2, 3])
+# Plotting the line graphs
+for i, (specialty, values) in enumerate(data.items()):
+    years = list(values.keys())
+    frequencies = list(values.values())
+    plt.plot(years, frequencies, color=colors[i % len(colors)], label=specialty)
 
-# marker|line|color
-plt.plot(xpoints1, ypoints1, marker='o', c='#4caf50')
-plt.plot(xpoints1, ypoints2, marker='o', c='#f4f5ad')
+plt.xlabel('Year')
+plt.ylabel('Frequency')
+plt.legend()
+plt.title('Specialty Frequencies Over Time')
+plt.grid(True)
 plt.show()
